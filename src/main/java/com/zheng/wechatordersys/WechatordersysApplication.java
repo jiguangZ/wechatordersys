7cea408306f18a148a033b8b45cd3dcb25097e08
jiguangZ <18851195238@163.com>
2018-08-05 15:05:19 +0800

    提交

---
+++ src/main/java/com/zheng/wechatordersys/WechatordersysApplication.java
@@ -0,0 +1,12 @@
+package com.zheng.wechatordersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatordersysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatordersysApplication.class, args);
+    }
+}
